803f5b8fc01fc8bd45442a9720a2c5b69c2194e7
aosun <956534763@qq.com>
2021-10-17 21:02:40 +0800

    初始化项目结构

---
+++ gulimall-produck/src/main/java/com/atguigu/gulimall/produck/GulimallProduckApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.produck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallProduckApplication.class, args);
+	}
+
+}
---
+++ gulimall-produck/src/test/java/com/atguigu/gulimall/produck/GulimallProduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.produck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
